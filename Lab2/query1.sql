bcc54054eef379ddff3183522237ff896ba0dcbe
Evin Odisho <edodisho@ucsc.edu>
2022-04-24 23:21:23 -0700

    First query made and tested, and appears to work

---
+++ Lab2/query1.sql
@@ -0,0 +1,6 @@
+-- Evin Odisho --
+-- edodisho --
+-- Query 1 --
+SELECT DISTINCT horseID, horseName, personName AS horseOwnerTrainerName
+FROM RacingPersons, Horses
+WHERE horseName IS NOT NULL AND horseOwnerID = trainerID;
