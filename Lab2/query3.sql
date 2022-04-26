1a05a61970af2f74e555c3a134ed2be4ae86ecd0
Evin Odisho <edodisho@ucsc.edu>
2022-04-25 22:24:47 -0700

    query 3 tested

--- Lab2/query3.sql
+++ Lab2/query3.sql
@@ -1,4 +1,7 @@
 SELECT hrr.horseID, hrr.raceDate, hrr.finishPosition, hrr.raceFinishTime - r.raceStartTime AS horseRaceInterval
 FROM HorseRaceResults hrr, Races r
-WHERE hrr.raceFinishTime - r.raceStartTime > TIME 00:02:00
-AND hrr.raceDate > DATE 2021-12-14
+WHERE hrr.raceFinishTime - r.raceStartTime > TIME '00:02:00'
+AND hrr.raceDate < DATE '2021-12-14'
+AND hrr.racetrackID = r.racetrackID
+AND hrr.raceDate = r.raceDate
+AND hrr.raceNum = r.raceNum
