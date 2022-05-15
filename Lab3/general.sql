4b6b4a81c868f11931ee55bed8234e658f0c2186
Evin Odisho <edodisho@ucsc.edu>
2022-05-15 00:43:23 -0700

    General has been implemented

---
+++ Lab3/general.sql
@@ -0,0 +1,17 @@
+-- Evin Odisho
+-- edodisho
+-- general.sql
+
+ALTER TABLE RaceTracks
+ADD CONSTRAINT positiveTrackDistance
+CHECK(trackDistance > 0 AND trackDistance IS NOT NULL);
+
+
+ALTER TABLE Horses
+ADD CONSTRAINT notBothOwnerTrainer
+CHECK(trainerID <> horseOwnerID)
+
+
+ALTER TABLE Races
+ADD CONSTRAINT bigChristmasPrize
+CHECK(raceDate != '2021-12-25' OR winningPrize > 1200); --If p then q is logically equivalent to: NOT p OR q
