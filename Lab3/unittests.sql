3e9b260b10df52f574739295b94047de59b17c63
Evin Odisho <edodisho@ucsc.edu>
2022-05-16 12:26:25 -0700

    First three tests for FK made

---
+++ Lab3/unittests.sql
@@ -0,0 +1,12 @@
+
+
+--fk Unit tests
+
+INSERT INTO Horses
+VALUES (553, 'Night Rider', 'A', DATE '2019-05-13', 15, 6008, 6007);
+
+INSERT INTO Horses
+VALUES (555, 'Essential Quality', 'M', DATE '2018-03-26', 1001, 16, 6008);
+
+INSERT INTO Horses
+VALUES (530, 'Easy Rider', 'M', DATE '2017-02-28', 1003, 6011, 11);
