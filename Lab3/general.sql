0694d849f902c8c556530a3f2eb67dc66c224e66
Evin Odisho <edodisho@ucsc.edu>
2022-05-16 12:26:10 -0700

    Quick changes

--- Lab3/general.sql
+++ Lab3/general.sql
@@ -5,7 +5,7 @@
 ALTER TABLE Racetracks
 ADD CONSTRAINT positiveTrackDistance
 CHECK(trackDistance > 0 AND trackDistance IS NOT NULL);
-
+--CHECK(trackDistance > 0);
 
 ALTER TABLE Horses
 ADD CONSTRAINT notBothOwnerTrainer
@@ -14,4 +14,4 @@ CHECK(trainerID <> horseOwnerID);
 
 ALTER TABLE Races
 ADD CONSTRAINT bigChristmasPrize
-CHECK(raceDate != '2021-12-25' OR winningPrize > 1200); --If p then q is logically equivalent to: NOT p OR q
+CHECK(raceDate != DATE '2021-12-25' OR (raceDate = DATE '2021-12-25' AND winningPrize > 1200)); --If p then q is logically equivalent to: NOT p OR q
