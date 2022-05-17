94d7f458b248a656c8735397c4a14d17a00d09b7
Evin Odisho <edodisho@ucsc.edu>
2022-05-17 12:29:08 -0700

    Query view is working now

--- Lab3/queryview.sql
+++ Lab3/queryview.sql
@@ -1,3 +1,16 @@
-SELECT DISTINCT horseID, horseName, lastCount, maxLastPlaceFinish
-FROM LastPlaceHorsesView
-WHERE (maxLastPlaceFinish >= 3);
+SELECT DISTINCT h.horseID, h.horseName, COUNT(hv.raceNum) AS lastCount, MAX(hv.finishPosition) AS maxLastPlaceFinish
+FROM LastPlaceHorsesView hv, Horses h
+WHERE h.horseID = hv.horseID
+GROUP BY h.horseID
+HAVING MAX(hv.finishPosition) >= 3;
+
+--  OUTPUT
+--  horseid |     horsename     | lastcount | maxlastplacefinish 
+-- ---------+-------------------+-----------+--------------------
+--      530 | Easy Rider        |         1 |                  5
+--      550 | Tiz the Law       |         2 |                  3
+--      551 |                   |         4 |                  5
+--      555 | Essential Quality |         4 |                  7
+--      575 |                   |         2 |                  3
+-- (5 rows)
+
