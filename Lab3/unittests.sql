f1cc8787d552bae0829fd3b406e562d924d164f5
Evin Odisho <edodisho@ucsc.edu>
2022-05-17 00:31:11 -0700

    Testing unittest

--- Lab3/unittests.sql
+++ Lab3/unittests.sql
@@ -10,3 +10,42 @@ VALUES (555, 'Essential Quality', 'M', DATE '2018-03-26', 1001, 16, 6008);
 
 INSERT INTO Horses
 VALUES (530, 'Easy Rider', 'M', DATE '2017-02-28', 1003, 6011, 11);
+
+
+--general Unit tests
+
+-- meets constraint for first positveTrackDistance Constraint
+UPDATE Racetracks
+SET trackDistance = trackDistance
+WHERE (trackDistance > 0);
+
+-- violates constraint for first positveTrackDistance Constraint
+UPDATE Racetracks
+SET trackDistance = -1
+WHERE (trackDistance < 0);
+
+
+
+-- meets constraint for notBothOwnerTrainer
+UPDATE Horses
+SET trainerID = trainerID
+WHERE (trainerID = horseOwnerID);
+
+-- violates constraint for notBothOwner Trainer
+UPDATE Horses
+SET trainerID = 1;
+WHERE (trainerID != horseOwnerID);
+
+
+
+-- meets constraint for bigChristmasPrize Constraint
+UPDATE Races
+SET winningPrize = winningPrize, raceDate = raceDate
+WHERE (winningPrize > 1200 and raceDate = DATE '2021-12-25');
+
+--violates constraint for bigChristmasPrize constraint
+UPDATE Races
+SET winningPrize = 100, raceDate = DATE '2022-1-10';
+WHERE (winningPrize <= 1200 AND raceDate != DATE '2021-12-25');
+
+
